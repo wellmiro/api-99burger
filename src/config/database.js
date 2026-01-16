63f5bf90cfe8d6dce535a616aeeea03c3d67b63a
wellmiro <wellingtonmiranda23@yahoo.com>
2026-01-16 14:31:40 -0300

    Correção: Trocando connection por pool para evitar erro de banco fechado

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+// Usamos createPool para que a conexão se recupere sozinha de quedas
+const db = mysql.createPool({
     host: "mysql-30331302-burgers-api-99.e.aivencloud.com",
     port: 20808,
     user: "avnadmin",
@@ -8,14 +9,19 @@ const db = mysql.createConnection({
     database: "defaultdb",
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect(function(err){
-    if (err){
+// No Pool, verificamos a conexão assim:
+db.getConnection((err, connection) => {
+    if (err) {
         console.log("Erro ao conectar com o banco da Nuvem:", err.message);
     } else {
         console.log("Conectado com sucesso ao banco Aiven (AWS)!");
+        connection.release(); // Libera a conexão para o pool
     }
 });
 
